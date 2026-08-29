87474b3fdc434f346d363b9aca370ab7458625f5
zovoik-byte <zovoik@learners.sjr.mb.ca>
2026-08-29 14:26:38 -0500

    added changing colors

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ button.addEventListener("click", () => {
     cheers = cheers + 1;
     count.textContent = cheers + " cheers";
   });
+
+  colors = ["red", "yellow", "green", "pink"]
+  colors = 0;
+  window.setInterval(() => {
+    color = (color + 1) % colors.length;
+    document.querySelector("#color-change").style.color
+  },1000)
+
